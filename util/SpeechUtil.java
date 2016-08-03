d657331c4028063ec4591a252ba1e2158186cbe3
zoehneto <tom.zoehner@gmail.com>
2016-08-03 11:19:48 +0200

    Use lambdas and remove unnecessary exceptions

--- util/SpeechUtil.java
+++ util/SpeechUtil.java
@@ -2,7 +2,6 @@ package de.dhbw.wwi13b.shared.util;
 
 import com.aldebaran.qi.CallError;
 import com.aldebaran.qi.Session;
-import com.aldebaran.qi.helper.EventCallback;
 import com.aldebaran.qi.helper.proxies.ALMemory;
 import com.aldebaran.qi.helper.proxies.ALSpeechRecognition;
 
@@ -10,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * Created by j on 7/27/16.
@@ -54,10 +54,8 @@ public class SpeechUtil {
      * @param words
      * @param callback
      * @return
-     * @throws CallError
-     * @throws InterruptedException
      */
-    public Thread onSpeech(List<String> words, SpeechRecognitionCallback callback) throws CallError, InterruptedException {
+    public Thread onSpeech(List<String> words, Function<String, Void> callback) {
         return this.onSpeech(words, 5, callback);
     }
 
@@ -68,35 +66,22 @@ public class SpeechUtil {
      * @param seconds
      * @param callback
      * @return
-     * @throws InterruptedException
-     * @throws CallError
      */
-    public Thread onSpeech(List<String> words, int seconds, SpeechRecognitionCallback callback) throws InterruptedException, CallError {
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println("IN HERE");
-                    speech.setWordListAsVocabulary(words);
-                    speech.subscribe("BLA");
-                    memory.subscribeToEvent("WordRecognized", new EventCallback() {
-                        @Override
-                        public void onEvent(Object o) throws InterruptedException, CallError {
-                            ArrayList<Object> list = (ArrayList<Object>)o;
-                            callback.onSpeech((String)list.get(0));
-                            System.out.println((String)list.get(0));
-                        }
-                    });
-                    Thread.sleep(seconds * 1000);
-                    speech.unsubscribe("BLA");
-                } catch (CallError callError) {
-                    callError.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    public Thread onSpeech(List<String> words, int seconds, Function<String, Void> callback) {
+        Thread thread = new Thread(() -> {
+            try {
+                System.out.println("IN HERE");
+                speech.setWordListAsVocabulary(words);
+                speech.subscribe("BLA");
+                memory.subscribeToEvent("WordRecognized", o -> {
+                    ArrayList<Object> list = (ArrayList<Object>)o;
+                    callback.apply((String)list.get(0));
+                    System.out.println((String)list.get(0));
+                });
+                Thread.sleep(seconds * 1000);
+                speech.unsubscribe("BLA");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
 
