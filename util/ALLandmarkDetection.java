aa38497e0413bce38f5d370c733f9312814cb263
Christian Huck <c.huck@highq.de>
2016-08-22 11:21:10 +0200

    fixed landmarks

--- util/ALLandmarkDetection.java
+++ util/ALLandmarkDetection.java
@@ -1,7 +1,162 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package de.dhbw.wwi13b.shared.util;
 
-/**
- * Created by huck on 22.08.16.
- */
-public class ALLandmarkDetection {
+import com.aldebaran.qi.CallError;
+import com.aldebaran.qi.Future;
+import com.aldebaran.qi.Session;
+import com.aldebaran.qi.helper.ALProxy;
+import java.util.List;
+
+public class ALLandMarkDetection extends ALProxy {
+    private ALLandMarkDetection.AsyncALLandmarkDetection asyncProxy = new ALLandMarkDetection.AsyncALLandmarkDetection();
+
+    public ALLandMarkDetection(Session session) throws Exception {
+        super(session);
+        System.out.print("LMD has name"+super.getService());
+        this.asyncProxy.setService(this.getService());
+    }
+
+    public ALLandMarkDetection.AsyncALLandmarkDetection async() {
+        return this.asyncProxy;
+    }
+
+
+    public void subscribe(String name, Integer period, Float precision) throws CallError, InterruptedException {
+        this.call("subscribe", new Object[]{name, period, precision}).get();
+    }
+
+    public void subscribe(String name) throws CallError, InterruptedException {
+        this.call("subscribe", new Object[]{name}).get();
+    }
+
+    public void unsubscribe(String name) throws CallError, InterruptedException {
+        this.call("unsubscribe", new Object[]{name}).get();
+    }
+
+    public void updatePeriod(String name, Integer period) throws CallError, InterruptedException {
+        this.call("updatePeriod", new Object[]{name, period}).get();
+    }
+
+    public void updatePrecision(String name, Float precision) throws CallError, InterruptedException {
+        this.call("updatePrecision", new Object[]{name, precision}).get();
+    }
+
+    public Integer getCurrentPeriod() throws CallError, InterruptedException {
+        return (Integer)this.call("getCurrentPeriod", new Object[0]).get();
+    }
+
+    public Float getCurrentPrecision() throws CallError, InterruptedException {
+        return (Float)this.call("getCurrentPrecision", new Object[0]).get();
+    }
+
+    public Integer getMyPeriod(String name) throws CallError, InterruptedException {
+        return (Integer)this.call("getMyPeriod", new Object[]{name}).get();
+    }
+
+    public Float getMyPrecision(String name) throws CallError, InterruptedException {
+        return (Float)this.call("getMyPrecision", new Object[]{name}).get();
+    }
+
+
+    public List<String> getOutputNames() throws CallError, InterruptedException {
+        return (List)this.call("getOutputNames", new Object[0]).get();
+    }
+
+    public List<String> getEventList() throws CallError, InterruptedException {
+        return (List)this.call("getEventList", new Object[0]).get();
+    }
+
+    public List<String> getMemoryKeyList() throws CallError, InterruptedException {
+        return (List)this.call("getMemoryKeyList", new Object[0]).get();
+    }
+
+
+    public class AsyncALLandmarkDetection extends ALProxy {
+        protected AsyncALLandmarkDetection() {
+        }
+
+
+
+        public Future<Boolean> setActiveCamera(Integer cameraId) throws CallError, InterruptedException {
+            return this.call("setActiveCamera", new Object[]{cameraId});
+        }
+
+        public Future<Boolean> isProcessing() throws CallError, InterruptedException {
+            return this.call("isProcessing", new Object[0]);
+        }
+
+        public Future<Void> pause(Boolean paused) throws CallError, InterruptedException {
+            return this.call("pause", new Object[]{paused});
+        }
+
+
+
+        public Future<List<String>> getMethodList() throws CallError, InterruptedException {
+            return this.call("getMethodList", new Object[0]);
+        }
+
+
+
+        public Future<Boolean> isRunning(Integer id) throws CallError, InterruptedException {
+            return this.call("isRunning", new Object[]{id});
+        }
+
+
+        public Future<Void> subscribe(String name, Integer period, Float precision) throws CallError, InterruptedException {
+            return this.call("subscribe", new Object[]{name, period, precision});
+        }
+
+        public Future<Void> subscribe(String name) throws CallError, InterruptedException {
+            return this.call("subscribe", new Object[]{name});
+        }
+
+        public Future<Void> unsubscribe(String name) throws CallError, InterruptedException {
+            return this.call("unsubscribe", new Object[]{name});
+        }
+
+        public Future<Void> updatePeriod(String name, Integer period) throws CallError, InterruptedException {
+            return this.call("updatePeriod", new Object[]{name, period});
+        }
+
+        public Future<Void> updatePrecision(String name, Float precision) throws CallError, InterruptedException {
+            return this.call("updatePrecision", new Object[]{name, precision});
+        }
+
+        public Future<Integer> getCurrentPeriod() throws CallError, InterruptedException {
+            return this.call("getCurrentPeriod", new Object[0]);
+        }
+
+        public Future<Float> getCurrentPrecision() throws CallError, InterruptedException {
+            return this.call("getCurrentPrecision", new Object[0]);
+        }
+
+        public Future<Integer> getMyPeriod(String name) throws CallError, InterruptedException {
+            return this.call("getMyPeriod", new Object[]{name});
+        }
+
+        public Future<Float> getMyPrecision(String name) throws CallError, InterruptedException {
+            return this.call("getMyPrecision", new Object[]{name});
+        }
+
+        public Future<Object> getSubscribersInfo() throws CallError, InterruptedException {
+            return this.call("getSubscribersInfo", new Object[0]);
+        }
+
+        public Future<List<String>> getOutputNames() throws CallError, InterruptedException {
+            return this.call("getOutputNames", new Object[0]);
+        }
+
+        public Future<List<String>> getEventList() throws CallError, InterruptedException {
+            return this.call("getEventList", new Object[0]);
+        }
+
+        public Future<List<String>> getMemoryKeyList() throws CallError, InterruptedException {
+            return this.call("getMemoryKeyList", new Object[0]);
+        }
+
+    }
 }
--- util/LMCoords.java
+++ util/LMCoords.java
@@ -1,7 +1,53 @@
 package de.dhbw.wwi13b.shared.util;
 
+import java.util.ArrayList;
+
 /**
  * Created by huck on 22.08.16.
  */
 public class LMCoords {
+    float alpha,beta,sizeX,sizeY,heading;
+    int landmarkID;
+
+    public float getAlpha() {
+        return alpha;
+    }
+
+    public float getBeta() {
+        return beta;
+    }
+
+    public float getSizeX() {
+        return sizeX;
+    }
+
+    public float getSizeY() {
+        return sizeY;
+    }
+
+    public float getHeading() {
+        return heading;
+    }
+
+    public int getLandmarkID() {
+        return landmarkID;
+    }
+
+    LMCoords(float alpha, float beta, float sizeX, float sizeY, float heading, int markID){
+        this.alpha = alpha;
+        this.beta = beta;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.heading = heading;
+        this.landmarkID = markID;
+    }
+
+    static LMCoords fromLandmarkInfo(ArrayList<Object> LMInfo){
+        ArrayList<Object> list1 =  (ArrayList<Object>) LMInfo.get(0);
+        ArrayList<Object> list2 =  (ArrayList<Object>) list1.get(0);
+        ArrayList<Object> list3 =  (ArrayList<Object>) list1.get(1);
+        return new LMCoords((float)list2.get(1),(float)list2.get(2),(float)list2.get(3),(float)list2.get(4),(int)list2.get(5),(int)list3.get(0));
+    }
+
 }
+
