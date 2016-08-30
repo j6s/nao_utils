894228211c5842b5952e72f2f9f4088b29429087
Christian Huck <c.huck@highq.de>
2016-08-30 15:23:53 +0200

    sonar

--- util/SonarUtil.java
+++ util/SonarUtil.java
@@ -1,9 +1,10 @@
 package de.dhbw.wwi13b.shared.util;
 
-import com.aldebaran.qi.CallError;
 import com.aldebaran.qi.Session;
+import com.aldebaran.qi.helper.proxies.ALMemory;
 import com.aldebaran.qi.helper.proxies.ALSonar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,20 +15,35 @@ public class SonarUtil {
      * ALMotion service instance
      */
     protected ALSonar sonar;
+    protected  ALMemory memory;
 
     public SonarUtil(Session session) throws Exception {
         this.sonar = new ALSonar(session);
+        this.memory = new ALMemory(session);
     }
 
-    public List<Float> getFilterdValues() throws Exception{
-        /**
-         * get FilteredValues, rightside, leftside
-         */
-        return sonar.getFilteredValues();
+
+    /**
+     * get distaces, leftside, rightside
+     */
+    public List<Float> getDistances() throws Exception{
+
+        sonar.subscribe("SONAR");
+
+        Object ol = memory.getData("Device/SubDeviceList/US/Left/Sensor/Value");
+        Object or = memory.getData("Device/SubDeviceList/US/Right/Sensor/Value");
+
+        sonar.unsubscribe("SONAR");
+
+        List<Float> values = new ArrayList<>();
+        values.add(Float.parseFloat(ol.toString()));
+        values.add(Float.parseFloat(or.toString()));
+
+        return values;
     }
 
-    public float getFilteredValuesMean() throws Exception {
-        List<Float> values = this.getFilterdValues();
+    public float getDistancesMean() throws Exception {
+        List<Float> values = this.getDistances();
         float total = 0;
         for (float val : values) {
             total += val;
