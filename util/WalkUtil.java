07383cf23d696f3f5832c0b7eb4cc3925ac633fc
Johannes Hertenstein <johannes@nimius.net>
2016-09-05 15:47:39 +0200

    FEATURE | WalkUtil: Add method for slow turning

--- util/WalkUtil.java
+++ util/WalkUtil.java
@@ -6,6 +6,9 @@ import com.aldebaran.qi.helper.proxies.ALMotion;
 import de.dhbw.wwi13b.shared.thread.Runnable;
 import de.dhbw.wwi13b.shared.thread.Thread;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Simple abstraction over walking and walking related tasks.
  * Provides an easy interface for walking related tasks that is
@@ -44,10 +47,57 @@ public class WalkUtil extends PostureUtil {
      * @throws InterruptedException
      * @throws CallError
      */
-    public void turnLeft(int degree) throws InterruptedException, CallError {
+    public void turnLeft(float degree) throws InterruptedException, CallError {
         float rad = MathUtil.deg2rad(degree);
+        log("turnLeft " + degree + "deg (" + rad + " rad)");
         this.motion.moveTo(0f, 0f, rad);
     }
+
+    /**
+     * Turns to the left, but slowly
+     * @param degree
+     * @throws InterruptedException
+     * @throws CallError
+     */
+    public void turnLeftSlow(int degree) throws InterruptedException, CallError {
+
+        // slight adjustment for to slow turning.
+        // TODO remove this
+        degree = (int) (degree * 1.25);
+        float theta = (float) ((degree / Math.abs(degree)) * .05);
+
+        List<List<Float>> steps = new ArrayList<>();
+        List<Float> step = new ArrayList<>();
+        step.add(0f); // X
+        step.add(0f); // Y
+        step.add(theta);
+
+        List<Float> s = new ArrayList<>();
+        s.add(0.01f);
+
+        List<String> legs = new ArrayList<>();
+        boolean leftLeg = true;
+
+        for (float i = 0; i < Math.abs(Math.toRadians(degree)); i += Math.abs(theta)) {
+            steps.add(step);
+            legs.add(leftLeg ? "LLeg" : "RLeg");
+            leftLeg = !leftLeg;
+        }
+
+        System.out.println(steps);
+        System.out.println(legs);
+
+        this.motion.setFootStepsWithSpeed(legs, steps, s, false);
+
+        while (this.motion.moveIsActive()) {
+            Thread.sleep(200);
+        }
+    }
+
+    public void turnRightSlow(int degrees) throws CallError, InterruptedException {
+        this.turnLeftSlow(-1 * degrees);
+    }
+
 
     /**
      * Turns left (Counterclockwise) by x degrees
@@ -117,6 +167,21 @@ public class WalkUtil extends PostureUtil {
 
         thread.start();
         return thread;
+    }
+
+    /**
+     * Turns the body in order to face a certain target
+     * The position is a list of coordinates in the form of [x, y]
+     * @param position
+     */
+    public void turnTowards(List<Float> position) throws CallError, InterruptedException {
+        log("turnTowards " + position);
+        float deg = (float) Math.toDegrees(Math.atan(position.get(1) / position.get(0)));
+        this.turnLeft(deg);
+    }
+
+    public ALMotion getMotion() {
+        return motion;
     }
 
 }
